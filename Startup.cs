126234068239d1570b906dbbb0a977aee5c4159a
idiotmohit <mohitjaiswal.bltr@gmail.com>
2019-08-20 23:06:55 -0700

    serverside code updated on AUG-20

---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TFS.Startup))]
+namespace TFS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
